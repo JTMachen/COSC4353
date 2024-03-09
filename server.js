e3302bdcf232f9084dfc079a77af532718d7361e
Jinn Ha <haphu155722@gmail.com>
2024-03-08 19:11:24 -0600

    added users.json example data. tweaked server.js

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = 5500;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-//When a client makes a GET request to this endpoint, the server will read the contents of the 'fuelquotehistory.json' file
-//Uses the fs.readFile() function
-//If an error occurs during reading, it will send a 500 status code and an error message
-//Otherwise, it parses the JSON data read from the file and sends it back to the client as a JSON response
-
+//Access fuel quote history JSON file
 app.get('/fuelquotehistory', (req, res) => {
     console.log('Received request to /fuelquotehistory endpoint');
     fs.readFile('fuelquotehistory.json', 'utf8', (err, data) => {
